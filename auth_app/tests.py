488b48c897eef3a0dc5f3b93f8a9cad1f5ea2e4d
shahriar350 <shahriar350@gmail.com>
2022-11-26 13:20:56 +0600

    requirements added

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -6,12 +6,7 @@ from .models import User
 
 
 class AnimalTestCase(TestCase):
-    def setUp(self):
-        User.objects.create_company(phone_number="01752495467",password="123456",name="Saifullah shahen")
+    def text_create_user(self):
+        user = User.objects.create_company(phone_number="01752495467", password="123456", name="Saifullah shahen")
+        self.assertEqual(user)
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
